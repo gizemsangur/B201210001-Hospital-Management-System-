20a80d005cd288a18bb466675fc5aa0a79c868e4
gizemsangur <103527969+gizemsangur@users.noreply.github.com>
2022-10-19 17:15:12 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+function show1(){
+    document.getElementById('display1').style.display="block"  
+    document.getElementById('display2').style.display="none" 
+    document.getElementById('display3').style.display="none" 
+    document.getElementById('display4').style.display="none" 
+}
+function show2(){
+    document.getElementById('display2').style.display="block"
+    document.getElementById('display1').style.display="none" 
+    document.getElementById('display3').style.display="none" 
+    document.getElementById('display4').style.display="none"    
+}
+function show3(){
+    document.getElementById('display3').style.display="block"
+    document.getElementById('display1').style.display="none" 
+    document.getElementById('display2').style.display="none" 
+    document.getElementById('display4').style.display="none"    
+}
+function show4(){
+    document.getElementById('display4').style.display="block"
+    document.getElementById('display1').style.display="none" 
+    document.getElementById('display2').style.display="none" 
+    document.getElementById('display3').style.display="none"   
+}
